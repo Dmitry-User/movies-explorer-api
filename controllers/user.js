b8f06ec129b75dad36663d83319eccc28bbeeea2
Dmitry Pavlov <dimapav1985@mail.ru>
2022-11-27 18:03:03 +0700

    fix: bug fix updateUser

--- controllers/user.js
+++ controllers/user.js
@@ -47,9 +47,9 @@ const createUser = (req, res, next) => {
 
 const updateUser = (req, res, next) => {
   const userId = req.user._id;
-  const { name, email } = req.body;
+  const { ...user } = req.body;
 
-  User.findByIdAndUpdate(userId, { name, email }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(userId, { ...user }, { new: true, runValidators: true })
     .orFail(new NotFoundError('Пользователь с указанным _id не найден'))
     .then((updatedUser) => {
       res.send(updatedUser);
