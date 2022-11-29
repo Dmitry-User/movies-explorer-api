f79bdbeb94ae0927c3768571715cd45cacf72387
Dmitry Pavlov <dimapav1985@mail.ru>
2022-11-29 16:53:48 +0700

    fix: fixed error message

--- utils/constants.js
+++ utils/constants.js
@@ -8,7 +8,7 @@ const authRequired = 'Необходима авторизация';
 const loginIncorrect = 'Неправильные почта или пароль';
 const userIdNotFound = 'Пользователь с указанным _id не найден';
 const userLogout = 'Вы вышли из профиля';
-const emailExists = 'Email уже зарегистрирован';
+const emailExists = 'Указанный email уже зарегистрирован';
 const routeNotFound = 'Запрашиваемый адрес не найден';
 const serverError = 'На сервере произошла ошибка';
 
